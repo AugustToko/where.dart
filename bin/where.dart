acc41bafab78eeb5f842a0462f63b3d8ce8306d3
Cédric Belin <cedric@belin.io>
2017-08-10 02:13:01 +0200

    Reverted the previous commit

---
+++ bin/where.dart
@@ -0,0 +1,9 @@
+#!/usr/bin/env dart
+
+import 'dart:async';
+import 'package:where/where.dart';
+
+/// Application entry point.
+Future main(List<String> arguments) async {
+  throw new UnimplementedError();
+}
