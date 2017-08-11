3ccd06978fd6bcaa3c99997c43f2fee821ce8683
Cédric Belin <cedric@belin.io>
2017-08-11 17:59:06 +0200

    Added the `Application` class

---
+++ lib/src/application.dart
@@ -0,0 +1,51 @@
+part of where;
+
+/// Represents an application providing functionalities specific to console requests.
+class Application {
+
+  /// The command line arguments.
+  final ArgParser _parser = new ArgParser()
+    ..addFlag('all', abbr: 'a', help: 'list all instances of executables found (instead of just the first one)', negatable: false)
+    ..addFlag('silent', abbr: 's', help: 'silence the output, just return the exit code (0 if any executable is found, otherwise 1)', negatable: false)
+    ..addFlag('help', abbr: 'h', help: 'output usage information', negatable: false)
+    ..addFlag('version', abbr: 'v', help: 'output the version number', negatable: false);
+
+  /// Prints the usage information.
+  String get usage => (new StringBuffer()
+    ..writeln('Find the instances of an executable in the system path.')
+    ..writeln()
+    ..writeln('Usage:')
+    ..writeln('pub global run where [options] <command>')
+    ..writeln()
+    ..writeln('Options:')
+    ..write(_parser.usage))
+    .toString();
+
+  /// Runs the application using the specified command line [arguments], and returns its exit code.
+  Future<int> run(List<String> arguments) async {
+    var results = _parser.parse(arguments);
+
+    if (results['help']) {
+      print(usage);
+      return 0;
+    }
+
+    if (results['version']) {
+      print(version);
+      return 0;
+    }
+
+    if (results.rest.isEmpty) {
+      print(usage);
+      return 2;
+    }
+
+    var paths = await where(results.rest.first, all: results['all']);
+    if (!results['silent']) {
+      if (paths is! List<String>) paths = [paths];
+      paths.forEach(print);
+    }
+
+    return 0;
+  }
+}
--- lib/where.dart
+++ lib/where.dart
@@ -3,9 +3,14 @@ library where;
 
 import 'dart:async';
 import 'dart:io' hide FileStat;
+import 'package:args/args.dart';
 import 'package:meta/meta.dart';
 import 'package:path/path.dart' as p;
 
+part 'src/application.dart';
 part 'src/file_stat.dart';
 part 'src/finder.dart';
 part 'src/where.dart';
+
+/// The version number of this package.
+const String version = '1.0.0';
