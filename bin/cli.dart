aee5c0bc17aad2a9a49cb91fec76b180ff6bf0e5
Cédric Belin <cedric@belin.io>
2017-08-11 16:55:25 +0200

    Renamed the CLI script

---
+++ bin/cli.dart
@@ -0,0 +1,21 @@
+#!/usr/bin/env dart
+
+import 'dart:async';
+import 'dart:io';
+import 'package:where/where.dart';
+
+/// Application entry point.
+Future main(List<String> arguments) async {
+  try {
+    exit(await new Application().run(arguments));
+  }
+
+  on FileSystemException {
+    exit(1);
+  }
+
+  on Exception catch (error) {
+    print(error);
+    exit(3);
+  }
+}
