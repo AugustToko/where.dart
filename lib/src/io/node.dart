c984570edf246d7f6eb3f6ec073d738540536a4c
Cédric Belin <cedric@belin.io>
2017-12-06 16:57:21 +0100

    Updated the dependencies

--- lib/src/io/node.dart
+++ lib/src/io/node.dart
@@ -1,34 +1,37 @@
 import 'dart:async';
 import 'dart:js';
-import 'package:nodejs_interop/nodejs_interop.dart';
+import 'package:file/file.dart';
+import 'package:nodejs_interop/nodejs_interop.dart' as node;
+import 'package:platform/platform.dart';
+import 'package:process/process.dart';
 import 'package:where/core.dart';
 
 /// The command line arguments.
-List<String> get arguments => process.argv.skip(2).toList();
+List<String> get arguments => node.process.argv.skip(2).toList();
 
 /// The global exit code for the process.
-int get exitCode => process.exitCode;
-set exitCode(int value) => process.exitCode = value;
+int get exitCode => node.exitCode;
+set exitCode(int value) => node.exitCode = value;
 
 /// A reference to the file system.
-const FileSystem fileSystem = const FileSystem();
+const FileSystem fileSystem = const node.FileSystem();
 
 /// A reference to the platform.
-const Platform platform = const Platform();
+const Platform platform = const node.Platform();
 
 /// A reference to the process manager.
-const ProcessManager processManager = const ProcessManager();
+const ProcessManager processManager = const node.ProcessManager();
 
 /// The numeric group identity of the process.
-Future<int> get processGid => new Future.value(platform.isWindows ? -1 : process.getgid());
+Future<int> get processGid => new Future.value(platform.isWindows ? -1 : node.process.getegid());
 
 /// The numeric user identity of the process.
-Future<int> get processUid => new Future.value(platform.isWindows ? -1 : process.getuid());
+Future<int> get processUid => new Future.value(platform.isWindows ? -1 : node.process.geteuid());
 
 /// Returns the statistics of the specified [file].
 Future<FileStats> getFileStats(String file) {
   var completer = new Completer<FileStats>();
-  loadLibrary<FSModule>('fs').stat(file, allowInterop((error, [stats]) {
+  node.loadLibrary<node.FSModule>('fs').stat(file, allowInterop((error, [stats]) {
     if (error != null) completer.completeError(new FileSystemException(error.message, file));
     else completer.complete(new FileStats(uid: stats.uid, gid: stats.gid, mode: stats.mode));
   }));
