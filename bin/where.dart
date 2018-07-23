f69256fdd2dce5065f292b4472730b409a512582
Cédric Belin <cedric@belin.io>
2018-07-23 14:20:44 +0200

    The CLI now uses the new `Options` class

--- bin/where.dart
+++ bin/where.dart
@@ -3,23 +3,15 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:isolate';
-import 'package:args/args.dart';
 import 'package:where/where.dart';
 import 'package:yaml/yaml.dart';
-
-/// The command line argument parser.
-final ArgParser _parser = ArgParser()
-  ..addFlag('all', abbr: 'a', help: 'List all instances of executables found (instead of just the first one).', negatable: false)
-  ..addFlag('silent', abbr: 's', help: 'Silence the output, just return the exit code (0 if any executable is found, otherwise 1).', negatable: false)
-  ..addFlag('help', abbr: 'h', help: 'Output usage information.', negatable: false)
-  ..addFlag('version', abbr: 'v', help: 'Output the version number.', negatable: false);
 
 /// The usage information.
 final String usage = (StringBuffer()
   ..writeln('Find the instances of an executable in the system path.')..writeln()
   ..writeln('Usage: where [options] <command>')..writeln()
   ..writeln('Options:')
-  ..write(_parser.usage))
+  ..write(argParser.usage))
   .toString();
 
 /// The version number of this package.
@@ -32,24 +24,24 @@ Future<String> get version async {
 /// Application entry point.
 Future<void> main(List<String> args) async {
   // Parse the command line arguments.
-  ArgResults results;
+  Options options;
 
   try {
-    results = _parser.parse(args);
-    if (results['help']) {
+    options = parseOptions(args);
+    if (options.help) {
       print(usage);
       return null;
     }
 
-    if (results['version']) {
+    if (options.version) {
       print(await version);
       return null;
     }
 
-    if (results.rest.isEmpty) throw ArgParserException('A command must be provided');
+    if (options.rest.isEmpty) throw const FormatException('A command must be provided.');
   }
 
-  on ArgParserException {
+  on FormatException {
     print(usage);
     exitCode = 64;
     return null;
@@ -57,8 +49,8 @@ Future<void> main(List<String> args) async {
 
   // Run the program.
   try {
-    var executables = await where(results.rest.first, all: results['all']);
-    if (!results['silent']) {
+    var executables = await where(options.rest.first, all: options.all);
+    if (!options.silent) {
       if (executables is! List<String>) executables = <String>[executables];
       executables.forEach(print);
     }
--- lib/where.dart
+++ lib/where.dart
@@ -7,6 +7,7 @@ import 'package:build_cli_annotations/build_cli_annotations.dart';
 import 'package:meta/meta.dart';
 import 'package:path/path.dart' as p;
 
+part 'where.g.dart';
 part 'src/cli.dart';
 part 'src/file_stats.dart';
 part 'src/finder.dart';
