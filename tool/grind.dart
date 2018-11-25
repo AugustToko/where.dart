5e4ec59adb9aa6f7da73d3f0db26edba81cf2e1f
Cédric Belin <cedric@belin.io>
2018-11-25 23:43:14 +0100

    Updated the build system

--- tool/grind.dart
+++ tool/grind.dart
@@ -11,7 +11,7 @@ void build() => Pub.run('build_runner', arguments: ['build', '--delete-conflicti
 void clean() {
   defaultClean();
   ['.dart_tool/build', 'doc/api', webDir.path].map(getDir).forEach(delete);
-  FileSet.fromDir(getDir('var'), pattern: '*.{info,json}').files.forEach(delete);
+  FileSet.fromDir(getDir('var'), pattern: '!.*', recurse: true).files.forEach(delete);
 }
 
 @Task('Uploads the results of the code coverage')
