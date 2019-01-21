dbe4eb208eeb85800cdb3e4e4fb3189b9088a770
Cédric Belin <cedric@belin.io>
2019-01-21 20:11:19 +0100

    Updated the Dart formatter settings

--- tool/grind.dart
+++ tool/grind.dart
@@ -25,7 +25,7 @@ Future<void> doc() async {
 }
 
 @Task('Fixes the coding standards issues')
-void fix() => DartFmt.format(existingSourceDirs, lineLength: 200);
+void fix() => DartFmt.format(existingSourceDirs);
 
 @Task('Performs the static analysis of source code')
 void lint() => Analyzer.analyze(existingSourceDirs);
