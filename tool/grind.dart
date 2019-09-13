be2578b603d4fa4696e10b400b46ca75d6160b25
Cédric Belin <cedric@belin.io>
2019-09-13 23:06:18 +0200

    Added a "publish" build task

--- tool/grind.dart
+++ tool/grind.dart
@@ -33,6 +33,10 @@ void fix() => DartFmt.format(existingSourceDirs);
 @Task('Performs the static analysis of source code')
 void lint() => Analyzer.analyze(existingSourceDirs);
 
+@Task('Publishes the package to the registry')
+@Depends(clean, fix)
+void publish() => run('pub', arguments: ['publish', '--force']);
+
 @Task('Runs the test suites')
 Future<void> test() => _profileTest('test/all.dart', 'var/lcov.info');
 
