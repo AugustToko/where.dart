b29ccc644a116238fcd211f57ea3cb523ab42d47
Cédric Belin <cedric@belin.io>
2020-06-08 18:39:10 +0200

    Updated the "publish" task

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -3,5 +3,6 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 tool/clean.ps1
+pub run build_runner build --delete-conflicting-outputs
 dartfmt --fix --overwrite bin lib test
 pub publish --force
